4b921ddfe81d22f30236314a895ae834291f50ee
Kodie <kodii.tulip@gmail.com>
2024-05-21 12:53:36 -0300

    💻✨Passive Generator Prototype
    adde new passive generator prototype
    + updated default label text for clicker prototype

---
+++ Scenes/Prototypes/Generator/prototype_generator.gd
@@ -0,0 +1,37 @@
+extends Control
+class_name PrototypeGenerator
+## Passive generator prototype, creating 1 stardust every second
+
+## Reference to the Button node
+@onready var button: Button = $Button
+## Reference to the Label node
+@onready var label: Label = $Label
+## reference to the Timer node
+@onready var timer: Timer = $Timer
+
+## Variable keeping track of the stardust amount
+var stardust: int = 0:
+	set(value):
+		stardust = value
+		_update_label()
+
+
+func _ready() -> void:
+	_update_label()
+
+## Method to update the label text
+func _update_label() -> void:
+	label.text = "Stardust : {value}".format({"value": stardust})
+
+## Method to create 1 stardust
+func _create_stardust() -> void:
+	stardust += 1
+
+
+func _on_button_pressed() -> void:
+	timer.start()
+	button.disabled = true
+
+
+func _on_timer_timeout() -> void:
+	_create_stardust()
